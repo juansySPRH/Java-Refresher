ebd48d1680fcd6319bf75445916e7189e88bc59f
John Otom <john.otom20@gmail.com>
2023-02-24 09:36:01 +0000

    Added: Basic Java Refresher

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    int Score = 10;
+    int targetScore = 100;
+
+    if (Score > 50) System.out.println("You got "+ Score +"/"+ targetScore);
+    else System.out.println("You Fail!");
+  }
+}
